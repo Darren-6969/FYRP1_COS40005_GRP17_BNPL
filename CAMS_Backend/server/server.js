32a63ff4488a1e89abf1b6d9aa1c8b2fd42b6888
Darren-6969 <102778736@students.swinburne.edu.my>
2026-04-06 16:46:12 +0800

    Add localhost:8080 to CORS and update lock
    Replace localhost:3000 with localhost:8080 in server CORS origins to permit development on port 8080. package-lock.json was regenerated to reflect current dependency resolution.

--- CAMS_Backend/server/server.js
+++ CAMS_Backend/server/server.js
@@ -99,7 +99,7 @@ app.use(cors({
   origin: [
     'https://fypbnpl.vercel.app',
     'http://localhost:5173',
-    'http://localhost:3000'
+    'http://localhost:8080'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
